10179d26c78ef89fc7e45237c1730aa7ca0ea871
ferly kurniawan <89645813+FerlyKurniawan@users.noreply.github.com>
2026-04-21 01:51:02 +0700

    Update __init__.py

--- pyrogram/storage/__init__.py
+++ pyrogram/storage/__init__.py
@@ -21,5 +21,8 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
+
+from .file_storage import FileStorage
+from .memory_storage import MemoryStorage
 from .sqlite_storage import SQLiteStorage
 from .storage import Storage
