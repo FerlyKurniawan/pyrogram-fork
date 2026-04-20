50d0a60f36ded1d427a7f67fcdf32b4ea41b9e70
ferly kurniawan <89645813+FerlyKurniawan@users.noreply.github.com>
2026-04-21 01:33:08 +0700

    Update __init__.py

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -41,3 +41,7 @@ from .client import Client
 from .sync import idle, compose
 
 crypto_executor = ThreadPoolExecutor(1, thread_name_prefix="CryptoWorker")
+
+from .utils import validate
+validate()
+
